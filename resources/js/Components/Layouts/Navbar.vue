26b2ef05da3d01477813290755e66cc9ef4f8d02
FenoAndria <feno.andriamorasata@gmail.com>
2023-03-27 15:40:41 +0300

    Auth : Logout

---
+++ resources/js/Components/Layouts/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-green-500 text-white font-bold p-4 flex justify-between">
+    <div class="space-x-4">
+      <router-link :to="{ name: 'Dashboard' }">Dashboard</router-link>
+    </div>
+    <div class="">
+      <span class="btn btn-sm btn-warning" @click="logout">Logout</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Navbar",
+  methods: {
+    logout() {
+      this.$store.dispatch("LOGOUT").then(() => {
+        this.$router.push({ name: "Login" });
+      });
+    },
+  },
+};
+</script>
