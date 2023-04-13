59ee99ff52d8da0d8224ceef8cb913d7eaafe51f
FenoAndria <feno.andriamorasata@gmail.com>
2023-04-13 19:34:07 +0300

    Login , Register : UI , Validation

--- resources/js/Views/Auth/Login.vue
+++ resources/js/Views/Auth/Login.vue
@@ -1,37 +1,55 @@
 <template>
-  <GuestLayout>
-    <h1>Login</h1>
-    <div class="">
-      <form @submit.prevent="login">
-        <div class="">
-          <label for="email">Email</label>
-          <input
-            class="input input-bordered input-sm"
-            type="text"
-            v-model="user.email"
-          />
+  <GuestLayout class="h-screen flex justify-center items-center">
+    <Card class="w-1/3 bg-green-500 py-16">
+      <h2 class="text-center text-gray-200 font-bold text-2xl mb-2">
+        Recruitment App
+      </h2>
+      <hr />
+      <h2 class="text-3xl text-white text-center mb-2">Login</h2>
+      <div class="">
+        <div class="" v-if="this.userNotAuthenticated">
+          <SingleError :error="feno"
+            >{{ this.userNotAuthenticated }}
+          </SingleError>
         </div>
-        <div class="">
-          <label for="password">Password</label>
-          <input type="text" v-model="user.password" />
-        </div>
-        <button class="btn btn-sm btn-success">Sign in</button>
-      </form>
-    </div>
-    <div class="">
-      <label
-        >Don't have an account?
-        <router-link
-          :to="{ name: 'Register' }"
-          class="text-teal-600 hover:text-teal-900"
-          >Register Now!</router-link
-        ></label
-      >
-    </div>
+        <form @submit.prevent="login">
+          <div class="x-form">
+            <ValidationError :errors="validationErrors" column="email" />
+            <label for="email" class="text-white">Email</label>
+            <input class="x-input" type="text" v-model="user.email" />
+          </div>
+          <div class="x-form">
+            <ValidationError :errors="validationErrors" column="password" />
+            <label for="password" class="text-white">Password</label>
+            <input class="x-input" type="text" v-model="user.password" />
+          </div>
+          <SubmitButton
+            :loading="this.loadingSubmit"
+            class="btn-success btn-block text-lg"
+          >
+            Sign in
+          </SubmitButton>
+        </form>
+      </div>
+      <div class="text-right">
+        <label class="text-white"
+          >Don't have an account ?
+          <router-link
+            :to="{ name: 'Register' }"
+            class="text-gray-600 hover:text-gray-900"
+            >Register Now!</router-link
+          ></label
+        >
+      </div>
+    </Card>
   </GuestLayout>
 </template>
 <script>
 import GuestLayout from "./../../Components/Layouts/GuestLayout.vue";
+import SubmitButton from "./../../Components/Layouts/SubmitButton.vue";
+import Card from "./../../Components/Layouts/Card.vue";
+import ValidationError from "./../../Components/Layouts/ValidationError.vue";
+import SingleError from "./../../Components/Layouts/SingleError.vue";
 
 export default {
   name: "Login",
@@ -41,25 +59,41 @@ export default {
         email: "",
         password: "",
       },
+      loadingSubmit: false,
+      validationErrors: "",
+      userNotAuthenticated: false,
     };
   },
   methods: {
     login() {
+      this.loadingSubmit = true;
+      this.validationErrors = '';
+      this.userNotAuthenticated = '';
       this.$store
         .dispatch("LOGIN", this.user)
         .then((result) => {
-          // console.log(result);
+          this.loadingSubmit = false;
           localStorage.setItem("userToken", result.data.user.token);
           location.reload();
-          // this.$router.push({ name: "Dashboard" });
         })
         .catch((err) => {
-          console.log(err.response);
+          this.loadingSubmit = false;
+          // console.log(err.response);
+          if (err.response.status === 422) {
+            this.validationErrors = err.response.data.errors;
+          }
+          if (err.response.status === 401) {
+            this.userNotAuthenticated = err.response.data.errors;
+          }
         });
     },
   },
   components: {
     GuestLayout,
+    Card,
+    SubmitButton,
+    ValidationError,
+    SingleError,
   },
 };
 </script>
