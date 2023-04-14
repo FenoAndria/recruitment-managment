6c3d8de9755619e9eae31eed31cea0d6643134ce
FenoAndria <feno.andriamorasata@gmail.com>
2023-04-14 19:52:56 +0300

    Company Profile UI : Index

--- resources/js/Views/Company/Profile/Index.vue
+++ resources/js/Views/Company/Profile/Index.vue
@@ -1,41 +1,60 @@
 <template>
   <CompanyLayout>
-    <div class="">
-      <h1 class="text-2xl">PROFILE</h1>
-      <div class="" v-if="loading">Loading profile...</div>
-      <div class="" v-else>
-        <div class="" v-if="company">
-          <p>Name : {{ company.name }}</p>
-          <p>
-            Address : {{ company.address }} {{ company.city }}
-            {{ company.country }}
-          </p>
-          <p>Phone : {{ company.phone }}</p>
-          <p>Website : {{ company.website }}</p>
-          <p>Description : {{ company.description }}</p>
-          <p>Brand :</p>
-          <img
-            :src="'http://localhost:8000' + company.brand"
-            alt="profile-image"
-            class="
-              w-52
-              h-52
-              object-cover
-              rounded-full
-              shadow-lg
-              border-2 border-maroon
-            "
-          />
-          <div class="">
-            <router-link :to="{ name: 'CompanyProfileEdit' }" class="btn btn-sm btn-success">Edit</router-link>
+    <div class="flex items-center justify-center">
+      <Card class="w-3/4 py-16">
+        <div class="" v-if="loading">Loading profile...</div>
+        <div class="" v-else>
+          <div
+            class="flex flex-wrap items-center justify-center"
+            v-if="company"
+          >
+            <div class="w-1/2">
+              <p class="text-3xl text-green-500 text-center font-semibold">
+                {{ company.name }}
+              </p>
+              <hr class="border-2 border-gray-200 my-1 rounded-lg" />
+              <div class="space-y-2">
+                <p>
+                  <i class="bi bi-geo-alt"></i> {{ company.address }}
+                  {{ company.city }}
+                  {{ company.country }}
+                </p>
+                <p><i class="bi bi-phone"></i> {{ company.phone }}</p>
+                <p><i class="bi bi-globe"></i> {{ company.website }}</p>
+                <p>
+                  <i class="bi bi-list-nested"></i> {{ company.description }}
+                </p>
+              </div>
+              <div class="float-right">
+                <router-link :to="{ name: 'CompanyProfileEdit' }" class="py-1 px-2 text-green-500 font-bold hover:shadow hover:bg-green-500 hover:text-white transition-all rounded">
+                  <i class="bi bi-pencil-square"></i>
+                </router-link>
+              </div>
+            </div>
+            <div class="w-1/2 flex justify-center items-center">
+              <img
+                :src="'http://localhost:8000' + company.brand"
+                alt="profile-image"
+                class="
+                  w-64
+                  h-64
+                  object-cover
+                  rounded-full
+                  shadow-lg
+                  border-2 border-maroon
+                "
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </Card>
     </div>
   </CompanyLayout>
 </template>
 <script>
 import CompanyLayout from "./../../../Components/Layouts/CompanyLayout.vue";
+import Card from "./../../../Components/Layouts/Card.vue";
+import Button from "./../../../Components/Layouts/Button.vue";
 import { mapGetters } from "vuex";
 export default {
   name: "CompanyProfile",
@@ -46,6 +65,8 @@ export default {
   },
   components: {
     CompanyLayout,
+    Card,
+    Button,
   },
   computed: {
     ...mapGetters(["company"]),
